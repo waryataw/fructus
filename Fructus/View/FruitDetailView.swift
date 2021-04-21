ff3b0e64e2282c5a00f20abce9f9a8f2f1512666
Anton Waryat <waryataw@gmail.com>
2021-04-21 09:31:13 +0300

    19. Learn how to create a detail view layout in SwiftUI 2.0 - Part 1

---
+++ Fructus/View/FruitDetailView.swift
@@ -0,0 +1,70 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Антон Варят on 21.04.2021.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(alignment: .center, spacing: 20) {
+                    // HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        // TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        // NUTRIENTS
+                        
+                        // SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        // LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                    } //: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                } //: VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            } //: SCROLL
+            .edgesIgnoringSafeArea(.top)
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREVIEW
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+            .previewDevice("iPhone 11 Pro")
+    }
+}
---
+++ Fructus/View/SourceLinkView.swift
@@ -0,0 +1,35 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Антон Варят on 21.04.2021.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        GroupBox {
+            HStack {
+                Text("Content source")
+                Spacer()
+                Link("Wikipedia",destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
